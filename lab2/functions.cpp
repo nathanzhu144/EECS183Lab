47fdc3afb81e7abf40bfb35b1a2dc5854e023fec
nathanzhu144 <znathan@umich.edu>
2018-09-20 08:24:33 -0400

    Update functions.cpp

--- lab2/functions.cpp
+++ lab2/functions.cpp
@@ -13,29 +13,33 @@ using namespace std;
 //Requires: Nothing.
 //Modifies: Nothing.
 //Effects:  Prints "Hello world!" without quotes. 
+void print_hello_world(){
+   cout << "Hello world!" << endl;
+   return;                             //This line is optional
+}
 
 //Requires: Nothing.
 //Modifies: Nothing.
 //Effects:  Returns the string "DOG" without quotes.
+string return_dog(){
+    return "DOG";
+}
 
 //Requires: Nothing.
 //Modifies: Nothing.
 //Effects:  Takes an int and cubes it, and returns that int.
-
-
-//Requires: Nothing.
-//Modifies: Nothing.
-//Effects:  What is it?
-void scope_challenge() {
-	int x = 3;
-
-	{
-		int x = 7;
-	}
+int cube_this_int(int number){
+    return number * number * number;
 }
 
 int main() {
-	scope_challenge();
+	print_hello_world();              //This line should print "Hello world!" without quotes
+	cout << return_dog() << endl;     //This line should print "DOG" without quotes.
+	return_dog();                     //This line should print nothing at all - why?
+	
+	//cout << print_hello_world();   //Why would this line result in a compiler error if uncommented?
+	
+	cout << cube_this_int(2) << endl; //This line should print 8.
 
 	return 0;
 }
